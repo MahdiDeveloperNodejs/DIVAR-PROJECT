6344b859c800bdc9f88cb7a0694127603b95cd9b
mahdi zare <mohemadzare13@gmail.com>
2026-09-24 03:06:13 -0700

    add view for project

--- src/app.routes.js
+++ src/app.routes.js
@@ -10,7 +10,7 @@ mainRouter.use("/user", UserRouter);
 mainRouter.use("/category", CategoryRouter);
 mainRouter.use("/option", OptionRouter);
 mainRouter.get("/", (req, res) => {
-    res.render("./pages/index.ejs");
+    res.render("./pages/panel/dashbord.ejs");
 }); 
 
 module.exports = mainRouter;
